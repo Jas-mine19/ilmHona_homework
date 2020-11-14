736c98c024caf0b0c139de26bab68308a6a1ba79
Zhasmina Shonazmieva <shonazmievajasmin@gmail.com>
2020-11-14 16:02:38 +0500

    was edit problem 2

--- lesson.11/problem.2/src/ITCC/another/goods.java
+++ lesson.11/problem.2/src/ITCC/another/goods.java
@@ -7,8 +7,10 @@ public class goods {
         public goods(int numOfClothes2, int numOfBoots2){
             this.numOfClothes=numOfClothes2;
             this.numOfBoots=numOfBoots2;
-
-
         }
+        public  void result(){
+            System.out.println(numOfClothes);
+            System.out.println(numOfBoots);
+    }
     }
 
